f3f33bcf83dc81e0bcafb0a996fcec5d90c99e0c
David Brossard <6230719+davidjbrossard@users.noreply.github.com>
2024-04-17 21:33:45 -0700

    Added log statements

--- src/translateXACMLResponse.js
+++ src/translateXACMLResponse.js
@@ -4,6 +4,7 @@ function translateXACMLResponse(xacmlResponse) {
   console.dir(xacmlResponse.data);
   let authZenResponse = allowed;
   try {
+    console.log('Should access be allowed?' + xacmlResponse.data.Response[0].Decision==='Permit');
     authZenResponse.decision = xacmlResponse.data.Response[0].Decision==='Permit';
   } catch (error) {
     console.log('Couldn\'t parse XACML Response');
